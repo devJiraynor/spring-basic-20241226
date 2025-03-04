e6dea7e23ef27269e4ca39a79c589a5715d630c0
devJiraynor <jiraynor@naver.com>
2025-03-04 17:11:19 +0900

    2025.03.04. 16:50

--- src/main/java/com/korit/basic/repository/UserRepository.java
+++ src/main/java/com/korit/basic/repository/UserRepository.java
@@ -8,5 +8,9 @@ import com.korit.basic.entity.UserEntity;
 // User 테이블에 접근할 리포지토리
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
+  // SELECT * FROM user WHERE user_id = ?;
+  UserEntity findByUserId(String userId);
 
+  // SELECT * FROM user WHERE user_tel_number = ?;
+  UserEntity findByUserTelNumber(String userTelNumber);
 }
