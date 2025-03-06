388199a14e33c8eae59efc7c850202d3ad3ce472
devJiraynor <jiraynor@naver.com>
2025-03-06 14:50:49 +0900

    2025.03.06. 14:50

--- src/main/java/com/korit/basic/service/implement/UserServiceImplement.java
+++ src/main/java/com/korit/basic/service/implement/UserServiceImplement.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.korit.basic.dto.GetUserListResponseDto;
 import com.korit.basic.dto.GetUserResponseDto;
 import com.korit.basic.dto.PostUserRequestDto;
 import com.korit.basic.dto.ResponseDto;
@@ -90,6 +91,11 @@ public class UserServiceImplement implements UserService {
 
     return ResponseDto.success(HttpStatus.CREATED);
 
+  }
+
+  @Override
+  public ResponseEntity<? super GetUserListResponseDto> getUserList() {
+    
   }
 
   @Override
